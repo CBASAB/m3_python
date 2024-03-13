ea25804b115de877a05aaaa8041145fb4a78a166
CBASAB <seba.riverosa@gmail.com>
2024-03-13 20:26:27 -0300

    Se agrega carpeta dia5 ejercicio5

---
+++ dia5/ejercicio5.py
@@ -0,0 +1,11 @@
+edad = 27
+#¿Juan es mayor de edad?
+print (edad >= 18)#True
+print (edad < 18)#False
+
+edad_graduacion_colegio = 17
+#¿Juan se graduo antes de los 18 años?
+print(edad_graduacion_colegio < 18) #True
+print(edad_graduacion_colegio >= 18) #false
+
+

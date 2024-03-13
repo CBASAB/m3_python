3013198d59e93e49d07fc67f3f0276a917766d96
CBASAB <seba.riverosa@gmail.com>
2024-03-13 19:50:40 -0300

    Se agrega print y archivo miprimerprograma

---
+++ dia1/ejemplo1/ejercicio1.py
@@ -0,0 +1,3 @@
+#Comentario de una sola linea
+"""Comentarios de mas de una linea"""
+print ("Hola g8")
---
+++ dia1/ejemplo1/mi_primer_programa.py
@@ -0,0 +1,10 @@
+nombre = "Sebastian"
+apellido1 = "Riveros"
+apellido2 = "Aliaga"
+edad = "34"
+#Python no permite sumar letras con numeros
+print ("****** Informacion *********")
+print ("**        " + nombre + "         **")
+print ("**        " + apellido1 + "      **")
+print ("**        " + apellido2 + "      **")
+print ("**        " + edad + "         **")

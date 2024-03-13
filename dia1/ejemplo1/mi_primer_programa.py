3587630c34b1016614ffdebe20c838d18127bb84
CBASAB <seba.riverosa@gmail.com>
2024-03-13 20:09:29 -0300

    Se agrega print de datos nombre, apellidos y edad

--- dia1/ejemplo1/mi_primer_programa.py
+++ dia1/ejemplo1/mi_primer_programa.py
@@ -2,9 +2,22 @@ nombre = "Sebastian"
 apellido1 = "Riveros"
 apellido2 = "Aliaga"
 edad = "34"
+año = 2024 #es un numero
 #Python no permite sumar letras con numeros
+#Suma de string = concatenar
 print ("****** Informacion *********")
-print ("**        " + nombre + "         **")
-print ("**        " + apellido1 + "      **")
-print ("**        " + apellido2 + "      **")
-print ("**        " + edad + "         **")
+print ("Tu nombre es " + nombre + "         **")
+print ("Tu apellido paterno es " + apellido1 + "      **")
+print ("Tu apellido materno es " + apellido2 + "      **")
+print ("Tu tienes la edad de " + edad + "         **")
+print ("el año es "+ año)
+print ("****************************")
+
+edad = 34
+direccion = "parcela 6"
+
+
+edad = 21.5
+
+
+edad = "50"

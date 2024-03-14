fea411f78083a65dba772401cdd7ef390ee5b1d4
CBASAB <seba.riverosa@gmail.com>
2024-03-13 21:13:28 -0300

    Se agregan preguntas para estudiar condicional y conector

--- dia5/ejercicio5.py
+++ dia5/ejercicio5.py
@@ -6,6 +6,31 @@ print (edad < 18)#False
 edad_graduacion_colegio = 17
 #¿Juan se graduo antes de los 18 años?
 print(edad_graduacion_colegio < 18) #True
-print(edad_graduacion_colegio >= 18) #false
+print(edad_graduacion_colegio >= 18) #False
+
+experiencia_laboral = 4
+#¿Juan NO tiene 4 años de experiencia laboral?
+print( experiencia_laboral != 4) #False
+print( experiencia_laboral == 4) #True
+
+numero_hijos = 0
+#¿Juan tien hijos? Tenemos varias formas de preguntar lo mismo
+print (numero_hijos > 0) #False
+print (numero_hijos == 0) #True
+print (numero_hijos < 1) #True
+print (numero_hijos != 0) #False
+print (numero_hijos >= 1) #False 
+# numero_hijos > 1 o numero_hijos = 1
+# False       v        False = False
 
 
+
+"""
+    1.- Si las exportaciones disminuyen entonces bajarán las utilidades -> Si las utilidades suben, las exportaciones aumentan
+    2.- Los precios son altos si y sólo sí los costos aumentan -> Si los costos aumentan, los precios aumentan
+    3.- Si la producción aumenta entonces bajarán los precios -> 
+    4.- Si la contaminación aumenta entonces existirá restricción vehicular adicional -> 
+    
+    5.- Ser o no ser
+    6.- La programacion no es facil -> Es dificil la programacion
+"""

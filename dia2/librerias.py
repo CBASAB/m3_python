b214b8cfa65bbb7a91e7d2579fe5b87f43c9b443
CBASAB <seba.riverosa@gmail.com>
2024-03-18 01:51:48 -0300

    Se agrega contando calorias

--- dia2/librerias.py
+++ dia2/librerias.py
@@ -12,6 +12,10 @@ print(5%2) #1, modulo: el resto de la división
 print(os.getcwd()) #C:\TalentoDigital\2023\0044-2\MODULO_PYTHON Me muestra la ruta donde estoy actualmente
 print(math.sqrt(9)) #3.0 Me entrega la raiz cuadrada del numero ingresado y lo retorna como un FLOAT
 #print(ceil(3.1416)) Importacion especifica de una libreria
-print(math.ceil(3.1416))
+print(math.ceil(3.1416)) #ceil aproxima al entero superior
 
+#pip
+#pip -V -> Para saber version
+#python -m pip install --upgrade pip ->Actualiza pip
+#pip install -> Para instalar librerias con pip
 

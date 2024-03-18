18c23f59be44c12ba0dbccace14316ad9ae2adba
CBASAB <seba.riverosa@gmail.com>
2024-03-18 12:54:42 -0300

    Se agrega validacion condicionales emprendedor3

--- dia3/ejercicio3/emprendedor3.py
+++ dia3/ejercicio3/emprendedor3.py
@@ -1,16 +1,24 @@
 #Captura de datos
 precio = int(input("Ingrese el precio del servicio en CLP: \n"))
 cantidad_usuarios = int(input("Ingrese la cantidad de usuarios: \n"))
-gastos_totales = int(input("Ingrese la cantidad de gastos totales \n"))
-utilidades_anio_anterior = int(input("Ingrese las utilidades del año anterior \n"))
+gastos_totales = int(input("Ingrese la cantidad de gastos totales en CLP: \n"))
+utilidades_anio_anterior = int(input("Ingrese las utilidades del año anterior en CLP (Mayores a 0): \n"))
 
+#Se realiza Calculo
+utilidades_actuales = (precio * cantidad_usuarios) - gastos_totales
+razon_utilidad_actual_y_anterior = (utilidades_actuales / utilidades_anio_anterior)
+
+#Validación de variables (Condicional)
 
 """
 if utilidades_anio_anterior > 0 :
-
+    #Realizar lo que este aqui tabulado
 else:
     #realizar accion por default
     print("Las utilidades no pueden ser iguales a cero")
     """
-    
-#Paso3 : Print de datos
+
+#Print de datos
+print(f"La razón entre la utilidad actual vs la utilidad del año anterior es: {round(razon_utilidad_actual_y_anterior,2)} de crecimiento")
+
+

6e9ce2214b566ad9357c47657b7f911d9eb1ccf1
CBASAB <seba.riverosa@gmail.com>
2024-03-17 22:59:00 -0300

    Se agrega dia2

---
+++ dia2/ejercicio2.py
@@ -0,0 +1,7 @@
+#La separacion de los float es un punto (.)
+
+#La division entre integers (Enteros), el resultado es un Float
+
+print(4/2) #Aun cuando los numero son enteros el resultado sera un flotante 2.0
+print(5/2) #2.5
+print(5/3.5)

7bc00ee9b96720466000df47fbc2eb6eb6a1434c
CBASAB <seba.riverosa@gmail.com>
2024-03-17 22:47:19 -0300

    Se agrega interpolacion en strings

--- dia1/ejemplo1/mi_primer_programa.py
+++ dia1/ejemplo1/mi_primer_programa.py
@@ -10,14 +10,18 @@ print ("Tu nombre es " + nombre + "         **")
 print ("Tu apellido paterno es " + apellido1 + "      **")
 print ("Tu apellido materno es " + apellido2 + "      **")
 print ("Tu tienes la edad de " + edad + "         **")
-print ("el año es "+ año)
+#Imprimiendo string y numeros en una misma linea
+print ("el año es ", año, " mes",3," Dia",7)
 print ("****************************")
 
 edad = 34
 direccion = "parcela 6"
-
-
 edad = 21.5
-
-
 edad = "50"
+
+#Interpolacion de cadenas (otra forma de imprimir en un solo print) (f) print (f"")
+mes = 3
+dia = 7
+#Dentro del string estoy poniendo los valores de las variables que cree
+print (f"Hola {nombre}, El año es {año} del mes {mes} y el dia {dia}")
+

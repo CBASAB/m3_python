4b0820c04fe1bfbd8e1a7c89e07792d6eaadc7b2
CBASAB <seba.riverosa@gmail.com>
2024-03-18 22:55:52 -0300

    Condicional else

--- dia6/condicionales.py
+++ dia6/condicionales.py
@@ -8,4 +8,21 @@
 edad = input("¿Que edad tienes?\n")#20 -> Se almacena "20"
 edad = int(edad)# edad = int("20") -> edad = 20
 
-#Si el usuario es mayor de edad
+#Si el usuario es mayor de edad o menor de edad
+if edad >= 18:
+    print("Eres mayor de edad")
+print("Programa terminado")
+
+#ELSE, para mas de una opcion como respuesta o respuesta que queda por default
+
+#if condicion :
+    #codigo a ejecutar (tabulado a la derecha)
+#else:
+    #(entonces) Ejecutar el siguiente codigo
+    
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+print("Programa terminado2")

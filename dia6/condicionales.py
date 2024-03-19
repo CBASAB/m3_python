9bed6ccc8a63f62231ee346dd4677d820c6efa45
CBASAB <seba.riverosa@gmail.com>
2024-03-18 22:40:29 -0300

    Se agrega carpeta dia6 y se comienza con condicional if

---
+++ dia6/condicionales.py
@@ -0,0 +1,11 @@
+#condicional if
+
+#Si se cumple la condicion (True), 
+# entonces se ejecuta el codigo
+#if condicion :
+    #codigo a ejecutar (tabulado a la derecha)
+    
+edad = input("¿Que edad tienes?\n")#20 -> Se almacena "20"
+edad = int(edad)# edad = int("20") -> edad = 20
+
+#Si el usuario es mayor de edad

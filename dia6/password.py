73b7b5e143ffeb3f207dccd39adada619f79197d
CBASAB <seba.riverosa@gmail.com>
2024-03-19 23:19:49 -0300

    Ejercicio password

---
+++ dia6/password.py
@@ -0,0 +1,19 @@
+#Captura de datos
+contrasenia = input("Ingrese una contraseña (Min 6 caracteres):\n")
+
+#string.count("")
+if contrasenia.count(" ") > 0:
+    print("El password no puede contener espacios")
+elif contrasenia == "12345":
+    print("El password es incorrecto")
+elif len(contrasenia) < 6:
+    print("El password es demasiado corto")
+
+#Validar que si el usuario ingresa 12345 -> El password es incorrecto
+
+"""else:
+    #len -> Contar los caracteres que tenga el string
+    if len(contrasenia) < 6:
+        print("El password es demasiado corto")
+"""
+print("Programa terminado")

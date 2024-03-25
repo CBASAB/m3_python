a0b43828fcc454ff8be3081dd6c223233248c4a0
CBASAB <seba.riverosa@gmail.com>
2024-03-24 21:08:34 -0300

    Se agrega ejemplo sumando.py para sumar

---
+++ dia7/sumando.py
@@ -0,0 +1,7 @@
+i = 1 # primer valor a sumar
+suma = 0
+while i <= 100:
+ suma += i # acumulamos para la suma
+ i += 1 # incrementamos para sumar el siguiente valor
+
+print(f"El resultado final es {suma}")

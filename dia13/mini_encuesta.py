1e833b408a022c622597bb1b4cddd363092dd21f
CBASAB <seba.riverosa@gmail.com>
2024-03-26 20:46:30 -0300

    Se agrega dia13

---
+++ dia13/mini_encuesta.py
@@ -0,0 +1,4 @@
+
+preguntas = ['Enunciado Pregunta 1','Enunciado Pregunta 2','Enunciado Pregunta 3']
+respuestas = []
+
---
+++ dia13/mini_encuesta2.py
@@ -0,0 +1,18 @@
+def imprimir_menu():
+    print("Opciones: ")
+    print("1). De acuerdo")
+    print("2). En desacuerdo")
+    print("3). No me interesa")
+
+preguntas = ['Enunciado Pregunta 1','Enunciado Pregunta 2','Enunciado Pregunta 3']
+respuestas = []
+
+for pregunta in preguntas:
+    print(pregunta)
+    imprimir_menu()
+    respuestas.append(input("> \n"))
+    print("")
+
+for respuesta in 
+
+respuestas.append(input("< \n"))

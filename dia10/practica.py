b37de42efb9512e2c09cb98cfd17e44436ae3f39
CBASAB <seba.riverosa@gmail.com>
2024-03-27 00:36:31 -0300

    Se realiza ejercicio de practica diccionarios

---
+++ dia10/practica.py
@@ -0,0 +1,19 @@
+#Creacion del diccionario
+tecnologia = {
+    1: "celular",
+    2: "pc"
+}
+print(tecnologia)
+
+#Agregar elemento
+tecnologia[3] = "tablet"
+print(tecnologia)
+
+#Cambiar el valor a una clave
+tecnologia[2] = "mouse"
+print(tecnologia)
+
+#Eliminar clave:valor del diccionario
+del tecnologia[1]
+print(tecnologia)
+print("Fin del programa")
